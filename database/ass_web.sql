8fe0cebef83ce5c5437f2a2cc3343a87c6cf0a15
PhuLoi-1911545 <luonghuuphuloi11@gmail.com>
2021-11-28 21:06:04 +0700

    database final

--- database/ass_web.sql
+++ database/ass_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2021 at 09:10 AM
+-- Generation Time: Nov 28, 2021 at 03:05 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -41,10 +41,11 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
 (1, 'Pizza', 'Category_1139.jpg', 'Yes', 'Yes'),
-(2, 'Burger', 'Category_4919.jpg', 'Yes', 'Yes'),
+(2, 'Burger', 'Category_8858.jpg', 'Yes', 'Yes'),
 (12, 'Salad', 'Category_6737.jpg', 'Yes', 'Yes'),
 (13, 'Meat', 'Category_3494.jpg', 'Yes', 'Yes'),
-(17, 'Drink', 'Category_5552.jpg', 'No', 'Yes');
+(17, 'Drink', 'Category_5552.jpg', 'No', 'Yes'),
+(18, 'Dessert', 'Category_7622.jfif', 'No', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -72,13 +73,19 @@ INSERT INTO `food` (`id`, `title`, `description`, `price`, `image_name`, `catego
 (2, 'Veggie Pizza', 'Thanh nhẹ với ô liu đen tuyệt hảo, cà chua bi tươi ngon, nấm, thơm, bắp, hành và phô mai Mozzarella cho bạn bữa tiệc rau củ tròn vị​', '14.00', 'Food_6039.jpg', 1, 'No', 'Yes'),
 (9, 'Meat Pizza', 'Thơm ngon và giàu protein với thịt xông khói, xúc xích, thịt bò, giăm bông và pepperoni', '15.00', 'Food_1613.jpg', 1, 'No', 'Yes'),
 (10, 'Bacon King Burger', 'Bacon King Burger gồm hai miếng thịt bò nướng lửa mặn cùng thịt xông khói cắt dày, pho mát Mỹ nấu chảy và phủ trên một lớp tương cà và sốt mayonnaise kem.', '14.00', 'Food_6155.jpg', 2, 'No', 'Yes'),
-(11, 'Triple Cheese Whopper Burger', 'Triple Cheese Whopper Burger gồm ba miếng thịt bò nướng lửa thơm ngon, phủ pho mát Mỹ tan chảy, cà chua ngon ngọt, rau diếp tươi, sốt mayonnaise kem, tương cà, dưa chua giòn và hành tây trắng thái lát', '15.00', 'Food_8399.jpg', 2, 'Yes', 'Yes'),
+(11, 'Cheese Burger', 'Triple Cheese Whopper Burger gồm ba miếng thịt bò nướng lửa thơm ngon, phủ pho mát Mỹ tan chảy, cà chua ngon ngọt, rau diếp tươi, sốt mayonnaise kem, tương cà, dưa chua giòn và hành tây trắng thái lát', '15.00', 'Food_8399.jpg', 2, 'Yes', 'Yes'),
 (12, 'Chicken Cheese Burger', 'Chicken Cheese Burger gồm một lát gà chiên giòn, phô mát Thụy Sỹ, thịt xông khói và sốt kem trứng', '15.00', 'Food_2963.jpg', 2, 'No', 'Yes'),
 (13, 'Italian Salad', 'Với công thức dầu giấm đặc biệt, thơm dịu nhẹ và giàu hương vị, các loại nguyên liệu khác nhau trong salad Italian được hoà trộn một cách hài hoà, lan toả.', '11.00', 'Food_3547.jpg', 12, 'Yes', 'Yes'),
 (15, 'Niçoise Salad', 'Niçoise Salad - Cá Ngừ Ngâm Dầu\r\nNhóm: salad truyền thống\r\nĐến từ miền Nam nước Pháp, salad Niçoise kết hợp rau quả tươi ngon cùng các loại nguyên liệu khác để tạo nên sự cân bằng giữa vitamin và protein.', '12.00', 'Food_8544.jpg', 12, 'No', 'Yes'),
 (17, 'Caesar Salad', 'Vị cá mặn đặc trưng, bánh mì giòn rụm, phô mai thơm mềm, tất cả kết hợp hài hoà trong món salad Caesar đã nổi tiếng từ lâu.', '12.00', 'Food_7474.jpg', 12, 'No', 'Yes'),
 (18, 'Monte Steak', 'Monte là dòng thịt bò cao cấp có nguồn gốc độc quyền của The Meat & Wine được nướng đến mức hoàn hảo ', '14.00', 'Food_6471.jpg', 13, 'Yes', 'Yes'),
-(19, 'Lamb Rib Steak', 'Sườn cừu đến từ Bắc Âu được nấu theo công thức người Viking', '14.00', 'Food_5706.jpg', 13, 'No', 'Yes');
+(19, 'Lamb Rib Steak', 'Sườn cừu đến từ Bắc Âu được nấu theo công thức người Viking', '14.00', 'Food_5706.jpg', 13, 'No', 'Yes'),
+(20, 'Chocolate Cheesecake', 'Bánh pho mát nướng với nhân kem sô cô la và lớp vỏ bơ.', '8.00', 'Food_5165.jpeg', 18, 'Yes', 'Yes'),
+(21, 'Molten Chocolate Cake', 'Bánh sô cô la với phần trên là sô cô la nóng chảy, phủ kem vani ở giữa và lớp bánh so cô la ở dưới', '8.00', 'Food_4554.jpg', 18, 'No', 'Yes'),
+(22, 'Creme Brulee', 'Bánh Creme Brulee với lớp đường đường ngào nâu màu cánh gián và nhân kem phô mai cùng quả dâu tằm', '9.00', 'Food_9791.jpg', 18, 'No', 'Yes'),
+(23, 'Origin Boba', 'Trà sữa truyền thống với đường đen và trân châu từ Hồng Kông', '6.00', 'Food_6157.jpg', 1, 'Yes', 'Yes'),
+(24, 'Caramel Macchiato', 'Macchiato Caramel với hương vị ngọt nhẹ thanh thoát cùng lớp bọt sữa béo nhẹ', '7.00', 'Food_7725.jpg', 17, 'No', 'Yes'),
+(25, 'Cappuccino', 'Cappuccino của chúng tôi bắt đầu với cà phê espresso, sau đó thêm một lượng tương đương giữa sữa tươi và bọt sữa cho thật hấp dẫn.', '6.00', 'Food_3931.jpg', 17, 'No', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -106,8 +113,13 @@ CREATE TABLE `food_order` (
 --
 
 INSERT INTO `food_order` (`id`, `food`, `price`, `quantity`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`, `note`) VALUES
-(13, '<span class=\"font-weight-bold\">1) </span>Monte Steak(x1); <span class=\"font-weight-bold\">2) </span>Italian Salad(x2); ', '25.00', 3, '75.00', '2021-11-28 14:54:11', 'Cancelled', 'Bach Khoa', '123456', 'bachkhoa@yahoo.com', '268 Ly THuong Kiet', 'salad khong co rau'),
-(14, '<span class=\"font-weight-bold\">1) </span>Triple Cheese Whopper Burger(x1); ', '15.00', 1, '15.00', '2021-11-28 15:04:31', 'Ordered', 'loi', '', '', '', '');
+(13, '<span class=\"font-weight-bold\">1) </span>Monte Steak(x1); <span class=\"font-weight-bold\">2) </span>Italian Salad(x2); ', '25.00', 3, '75.00', '2021-11-28 14:54:11', 'Delivered', 'Bach Khoa', '123456', 'bachkhoa@yahoo.com', '268 Ly THuong Kiet', 'salad khong co rau'),
+(14, '<span class=\"font-weight-bold\">1) </span>Triple Cheese Whopper Burger(x1); ', '15.00', 1, '15.00', '2021-11-28 15:04:31', 'Delivered', 'loi', '', '', '', ''),
+(15, '<span class=\"font-weight-bold\">1) </span>Triple Cheese Whopper Burger(x4); <span class=\"font-weight-bold\">2) </span>Cheese Pizza(x5); ', '27.00', 9, '243.00', '2021-11-28 17:09:33', 'Delivered', 'Luong Huu Phu Loi', '1911545', 'loi.luonglucasia@hcmut.edu.vn', '268 Ly thuong Kkiet', ''),
+(16, '<span class=\"font-weight-bold\">1) </span>Italian Salad(x4); ', '11.00', 4, '44.00', '2021-11-28 17:15:03', 'On Delivery', 'Bach Khoa', '1911545', 'bachkhoa@hcmut.edu.vn', '', 'salad khong co rau'),
+(17, '<span class=\"font-weight-bold\">1) </span>Triple Cheese Whopper Burger(x5); ', '15.00', 5, '75.00', '2021-11-28 17:15:29', 'Ordered', 'test guest', '123456', 'guest@yahoo.com', '', ''),
+(18, '<span class=\"font-weight-bold\">1) </span>Monte Steak(x1); <span class=\"font-weight-bold\">2) </span>Italian Salad(x1); ', '25.00', 2, '25.00', '2021-11-28 20:29:53', 'Ordered', 'Luong', '123456', 'testing@yahoo.com', '1665654', '565465459'),
+(19, '<span class=\"font-weight-bold\">1) </span>Chocolate New York Cheesecake(x1); ', '8.00', 1, '8.00', '2021-11-28 20:31:39', 'Ordered', 'Viet', '111111', 'prnhub@hub.com', '', '');
 
 -- --------------------------------------------------------
 
@@ -203,19 +215,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `food_order`
 --
 ALTER TABLE `food_order`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `manager`
