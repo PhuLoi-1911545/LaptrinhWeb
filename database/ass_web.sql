d1eaa78398e1e2a51ab7f91d979079316ca912b1
PhuLoi-1911545 <luonghuuphuloi11@gmail.com>
2021-11-28 15:14:16 +0700

    update database

--- database/ass_web.sql
+++ database/ass_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2021 at 05:43 AM
+-- Generation Time: Nov 28, 2021 at 09:10 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -44,8 +44,7 @@ INSERT INTO `category` (`id`, `title`, `image_name`, `featured`, `active`) VALUE
 (2, 'Burger', 'Category_4919.jpg', 'Yes', 'Yes'),
 (12, 'Salad', 'Category_6737.jpg', 'Yes', 'Yes'),
 (13, 'Meat', 'Category_3494.jpg', 'Yes', 'Yes'),
-(14, 'Drink', 'Category_6610.jpg', 'Yes', 'Yes'),
-(15, 'Dessert', 'Category_9429.jpg', 'Yes', 'Yes');
+(17, 'Drink', 'Category_5552.jpg', 'No', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -69,8 +68,17 @@ CREATE TABLE `food` (
 --
 
 INSERT INTO `food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
-(1, 'Burger Gà', 'Ngon tuyệt vời', '12.00', 'Food_4494.png', 2, 'Yes', 'Yes'),
-(2, 'Pizza Trứng', 'Bổ rẻ', '22.00', 'Food_4859.png', 1, 'Yes', 'Yes');
+(1, 'Cheese Pizza', 'Pizza Viền Phô Mai 2 Loại Nhân Phủ cùng xốt Cà Chua và phô Mai Mozzarella', '12.00', 'Food_5172.jpg', 1, 'Yes', 'Yes'),
+(2, 'Veggie Pizza', 'Thanh nhẹ với ô liu đen tuyệt hảo, cà chua bi tươi ngon, nấm, thơm, bắp, hành và phô mai Mozzarella cho bạn bữa tiệc rau củ tròn vị​', '14.00', 'Food_6039.jpg', 1, 'No', 'Yes'),
+(9, 'Meat Pizza', 'Thơm ngon và giàu protein với thịt xông khói, xúc xích, thịt bò, giăm bông và pepperoni', '15.00', 'Food_1613.jpg', 1, 'No', 'Yes'),
+(10, 'Bacon King Burger', 'Bacon King Burger gồm hai miếng thịt bò nướng lửa mặn cùng thịt xông khói cắt dày, pho mát Mỹ nấu chảy và phủ trên một lớp tương cà và sốt mayonnaise kem.', '14.00', 'Food_6155.jpg', 2, 'No', 'Yes'),
+(11, 'Triple Cheese Whopper Burger', 'Triple Cheese Whopper Burger gồm ba miếng thịt bò nướng lửa thơm ngon, phủ pho mát Mỹ tan chảy, cà chua ngon ngọt, rau diếp tươi, sốt mayonnaise kem, tương cà, dưa chua giòn và hành tây trắng thái lát', '15.00', 'Food_8399.jpg', 2, 'Yes', 'Yes'),
+(12, 'Chicken Cheese Burger', 'Chicken Cheese Burger gồm một lát gà chiên giòn, phô mát Thụy Sỹ, thịt xông khói và sốt kem trứng', '15.00', 'Food_2963.jpg', 2, 'No', 'Yes'),
+(13, 'Italian Salad', 'Với công thức dầu giấm đặc biệt, thơm dịu nhẹ và giàu hương vị, các loại nguyên liệu khác nhau trong salad Italian được hoà trộn một cách hài hoà, lan toả.', '11.00', 'Food_3547.jpg', 12, 'Yes', 'Yes'),
+(15, 'Niçoise Salad', 'Niçoise Salad - Cá Ngừ Ngâm Dầu\r\nNhóm: salad truyền thống\r\nĐến từ miền Nam nước Pháp, salad Niçoise kết hợp rau quả tươi ngon cùng các loại nguyên liệu khác để tạo nên sự cân bằng giữa vitamin và protein.', '12.00', 'Food_8544.jpg', 12, 'No', 'Yes'),
+(17, 'Caesar Salad', 'Vị cá mặn đặc trưng, bánh mì giòn rụm, phô mai thơm mềm, tất cả kết hợp hài hoà trong món salad Caesar đã nổi tiếng từ lâu.', '12.00', 'Food_7474.jpg', 12, 'No', 'Yes'),
+(18, 'Monte Steak', 'Monte là dòng thịt bò cao cấp có nguồn gốc độc quyền của The Meat & Wine được nướng đến mức hoàn hảo ', '14.00', 'Food_6471.jpg', 13, 'Yes', 'Yes'),
+(19, 'Lamb Rib Steak', 'Sườn cừu đến từ Bắc Âu được nấu theo công thức người Viking', '14.00', 'Food_5706.jpg', 13, 'No', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -80,7 +88,7 @@ INSERT INTO `food` (`id`, `title`, `description`, `price`, `image_name`, `catego
 
 CREATE TABLE `food_order` (
   `id` int(10) UNSIGNED NOT NULL,
-  `food` varchar(100) NOT NULL,
+  `food` varchar(500) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `quantity` int(11) NOT NULL,
   `total` decimal(10,2) NOT NULL,
@@ -98,13 +106,8 @@ CREATE TABLE `food_order` (
 --
 
 INSERT INTO `food_order` (`id`, `food`, `price`, `quantity`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`, `note`) VALUES
-(1, 'test22', '2.00', 3, '6.00', '2021-11-15 02:08:23', 'Delivered', 'Thanh Lương', '0916214675', 'caothanhluong56@gmail.com', '123, Phạm Hồng Thái, khu phố 4, phường Long Hoa, thị xã Hòa Thành, tỉnh Tây Ninh', 'Làm ngon ngon nha'),
-(2, 'Burger Gà', '12.00', 2, '24.00', '2021-11-21 02:06:07', 'Delivered', 'alo', '0916214677', 'caothanhluong56@gmail.com', 'ttttt', 'thêm tí đường'),
-(3, 'Burger Gà', '12.00', 1, '12.00', '2021-11-22 03:35:30', 'Ordered', 'alo', '0916214677', 'caothanhluong5226@gmail.com', 'tn', ''),
-(4, 'Pizza Trứng', '22.00', 10, '220.00', '2021-11-22 03:43:34', 'Delivered', 'Admin', '32323', 'luong.cao2202@hcmut.edu.vn', 'tn', 'nhiều nhiều muối'),
-(5, 'Pizza Trứng', '22.00', 10, '220.00', '2021-11-22 03:40:28', 'Ordered', 'PhuLoi', '0123456', 'loi.luong@yahoo.com', 'Bach KHoa', 'khong trung'),
-(6, 'Burger Gà', '12.00', 1, '12.00', '2021-11-22 03:41:49', 'Ordered', '123123qwe', 'qweqwe123', 'qweqasd@yahoo.com', 'asd1313w', 'Xin chào mọi người! Mình muốn lan tỏa năng lượng đăng kí thành công này đến mọi người. Và ai đã đăng kí thành công rồi có thể tìm bạn cùng khu cùng phòng bên dưới bình luận... Chúc mọi người buổi tối vui vẻ, ai chưa đăng kí được thì chúc đăng kí thành côn'),
-(7, '<span class=\"font-weight-bold\">1) </span>Pizza Trứng(x1); <span class=\"font-weight-bold\">2) </span>B', '34.00', 2, '34.00', '2021-11-25 10:26:53', 'Ordered', '', '', '', '', '');
+(13, '<span class=\"font-weight-bold\">1) </span>Monte Steak(x1); <span class=\"font-weight-bold\">2) </span>Italian Salad(x2); ', '25.00', 3, '75.00', '2021-11-28 14:54:11', 'Cancelled', 'Bach Khoa', '123456', 'bachkhoa@yahoo.com', '268 Ly THuong Kiet', 'salad khong co rau'),
+(14, '<span class=\"font-weight-bold\">1) </span>Triple Cheese Whopper Burger(x1); ', '15.00', 1, '15.00', '2021-11-28 15:04:31', 'Ordered', 'loi', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -124,8 +127,11 @@ CREATE TABLE `manager` (
 --
 
 INSERT INTO `manager` (`id`, `full_name`, `username`, `password`) VALUES
-(2, 'Thanh Luong', 'thanhluong', 'f288bd2c10f6fa568fa813222adf6ed8'),
-(3, 'Admin', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+(3, 'Admin', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(20, 'Phu Loi', 'loi', '84ab36b2995bb3949db34038a2b24c64'),
+(21, 'Luong', 'luong', 'cd67e095aa87a04d2bfb63a3b8f24309'),
+(22, 'Viet', 'viet', 'a4e614247683e150b2b9812a8fa33a71'),
+(23, 'Trung', 'trung', 'c2d8730c4cdd662573b0214a0183bf98');
 
 -- --------------------------------------------------------
 
@@ -150,11 +156,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `full_name`, `birthday`, `phone`, `email`, `rating`, `feedback`) VALUES
-(1, 'thanhluong', '9c1ad00a16a7c67e2727b471ac969e96', 'Cao Thanh Luong', '22/02/2001', '0916214675', 'caothanhluong56@gmail.com', 'Very Bad', ''),
-(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Adminnnn', '01/01/2022', '0916214671', 'caothanhluong2222@gmail.com', 'Bad', 'tôi thực sực rất hài lòng với cách làm việc của các bạntôi thực sực rất hài lòng với cách làm việc của các bạntôi thực sực rất hài lòng với cách làm việc của các bạn'),
-(8, 'alo', '1b2ccf52b54ea2c9468ca24fbe164919', 'alo', '01/01/2000', '0916214675', 'caothanhluong56@gmail.com', '', ''),
-(9, 'aloalo', 'bc6f0aa94f722407f66281abd0f4027c', 'Thanh Luong', '01/01/2022', '0916214675', 'caothanhluong56@gmail.com', '', ''),
-(10, 'myy', '5bbb16ac99f636dfac36a7c644732c88', 'Thanh Luong', '01/01/20222', '0916214675', 'caothanhluong56@gmail.com', '', '');
+(13, 'phuloi', '2e348a4f3b4335587ba2b181ee7b621b', 'Loi_guest', '03/05/2001', '1911545', 'loi.luonglucasia@hcmut.edu.vn', '', ''),
+(14, 'quangtrung', '19b0daf21b03f1eca638a683aab20200', 'Quang Trung', '6/9/2001', '1912330', 'trung.vu1912330@hcmut.edu.vn', 'Good', 'Cần phục vụ nhanh hơn'),
+(15, 'thanhluong', 'f288bd2c10f6fa568fa813222adf6ed8', 'Thang Luong', '1/1/2001', '914076', 'luong.cao2202@hcmut.edu.vn', '', ''),
+(16, 'vanviet', '080b6971159e2a47e8d0105719a9820e', 'Van Viet', '1/2/2001', '1912436', 'vanviet@hcmut.edu.vn', '', '');
 
 --
 -- Indexes for dumped tables
@@ -198,31 +203,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `food_order`
 --
 ALTER TABLE `food_order`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `manager`
 --
 ALTER TABLE `manager`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
